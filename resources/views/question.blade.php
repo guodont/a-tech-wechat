3b9f7f57e60621ff5c09a8ca990f3496097fe473
guodont <guodont@163.com>
2016-06-19 22:21:57 +0800

    完善问题详情页面5

--- resources/views/question.blade.php
+++ resources/views/question.blade.php
@@ -26,11 +26,34 @@
             <div class="weui_uploader_bd">
                 <ul class="weui_uploader_files">
                     @foreach(explode(',',$question->images) as $image)
-                        <li class="weui_uploader_file" style="background-image:url({{ $image }})"></li>
+                        <li class="weui_uploader_file" >
+                            <img src="{{ $image }}" alt="">
+                        </li>
                     @endforeach
                 </ul>
             </div>
         </article>
     </div>
-
+    @if ($question->answer != '')
+    <hr>
+    <div class="weui_panel_bd">
+        <a href="javascript:void(0);" class="weui_media_box weui_media_appmsg">
+            <div class="hd">
+                <img class="image" src="http://storage.workerhub.cn/default_avatar.png"/>
+            </div>
+            <div class="weui_media_bd">
+                <h4 class="weui_media_title">{{ $question->expert->realName }}</h4>
+                <p class="weui_media_desc">回答于{{ date('y-m-d H:i',$question->whenUpdated) }}</p>
+            </div>
+        </a>
+    </div>
+    <hr>
+    <article class="weui_article">
+        <section>
+            <p>
+                {{ $question->answer }}
+            </p>
+        </section>
+    </article>
+    @endif
 @endsection
