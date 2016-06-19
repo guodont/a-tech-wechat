39745e291721af3fb2452dbef1e759e57ff08e4a
guodont <guodont@163.com>
2016-06-19 22:12:49 +0800

    完善问题详情页面

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,8 +3,55 @@
     <title>@yield('title')</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link href="http://cdn.bootcss.com/weui/0.4.2/style/weui.css" rel="stylesheet">
-    <link rel="stylesheet" href="../../assets/css/style.css"
-          media="screen" type="text/css">
+    <style>
+        body,html{
+            height:100%;-webkit-tap-highlight-color:transparent
+        }
+        body{
+            overflow-x:hidden;background-color:#fbf9fe;    line-height: 1.6;
+            font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+        }
+        .page_title{
+            text-align: center;
+            font-size: 34px;
+            color: #3cc51f;
+            font-weight: 400;
+            margin: 0 15%;
+        }
+        div{
+            display: block;
+        }
+        *{
+            margin:0;
+            padding: 0;
+        }
+        .title{
+            padding-left:15px;
+            padding-top: 20px;
+        }
+        h4{
+            font-weight:normal !important;
+        }
+        .image{
+            width:100px;
+            height:100px;
+            border-radius:50px;
+        }
+        .hd .image{
+            width:50px;
+            height:50px;
+            border-radius:50px;
+        }
+        .weui_media_bd{
+            padding-left: 10px;
+        }
+        .weui_media_box{
+            padding-bottom:0px !important;
+        }
+        .hd {
+            padding: 5px !important;
+        }
+    </style>
 </head>
 <body>
 <div class="container">
