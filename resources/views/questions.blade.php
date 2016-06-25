3870a90c08f971895234c40e0576fcc17f9c9fab
guodont <guodont@163.com>
2016-06-25 21:44:05 +0800

    修改时间问题

--- resources/views/questions.blade.php
+++ resources/views/questions.blade.php
@@ -8,17 +8,25 @@
             <div class="weui_panel_hd">热门问题</div>
             <div class="weui_panel_bd">
                 @foreach ($questions as $question)
-                <a href="./question/{{ $question->id }}" class="weui_media_box weui_media_appmsg">
-                    <div class="weui_media_hd">
-                        <img class="weui_media_appmsg_thumb"
-                             src="{{ explode(',',$question->images)[0] }}"
-                             alt="">
-                    </div>
-                    <div class="weui_media_bd">
-                        <h4 class="weui_media_title">{{ $question->title }}</h4>
-                        <p class="weui_media_desc">{{ $question->user->name }} 发布于 {{ date('Y-m-d H:i',$question->whenCreated) }}</p>
-                    </div>
-                </a>
+                    <a href="./question/{{ $question->id }}" class="weui_media_box weui_media_appmsg">
+                        <div class="weui_media_hd">
+                            @if(explode(',',$question->images)[0] != null)
+                                <img class="weui_media_appmsg_thumb"
+                                     src="{{ explode(',',$question->images)[0] }}"
+                                     alt="">
+                            @else
+                                <img class="weui_media_appmsg_thumb"
+                                     src="http://storage.workerhub.cn/image/icon/icon_question.png"
+                                     alt="">
+                            @endif
+
+                        </div>
+                        <div class="weui_media_bd">
+                            <h4 class="weui_media_title">{{ $question->title }}</h4>
+                            <p class="weui_media_desc">{{ $question->user->name }}
+                                发布于 {{ date('r',$question->whenCreated) }}</p>
+                        </div>
+                    </a>
                 @endforeach
             </div>
         </div>
